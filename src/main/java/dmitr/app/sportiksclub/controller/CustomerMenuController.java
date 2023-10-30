a0c3e3598bdda6ab0d72149dd9246b1a990c5d68
theDmitr <dmitryfirson@gmail.com>
2023-10-30 11:39:29 +0300

    Menus for different roles

---
+++ src/main/java/dmitr/app/sportiksclub/controller/CustomerMenuController.java
@@ -0,0 +1,15 @@
+package dmitr.app.sportiksclub.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
